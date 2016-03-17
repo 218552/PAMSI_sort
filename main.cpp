7e888b347d68a4a090f7a73b3bd2d54ac4e8ac25
218492 <218492@student.pwr.edu.pl>
2016-03-18 00:37:41 +0100

    Added files via upload
    Zastosowanie konstruktorów i destruktorów zamiast funkcji. Przeniesienie odpowiednich funkcji do pól prywatnych.

--- main.cpp
+++ main.cpp
@@ -4,18 +4,38 @@ class  tabd{            /*tworzymy klase, w ktorej bedziemy zajmowac sie nasza t
 int rozmiar;            /*rozmiar tablicy*/
 int* w1;                /*wskaznik1*/
 int* w2;                /*wskaznik2*/
-public:
-void zrobtab(){             /*metoda pozwalajaca na stworzenie tablicy*/
-    rozmiar=10;             /*przyjmujemy rozmiar rowny 10*/
-    w1=new int[rozmiar];    /*alokujemy pamiec*/
-    }
-void zniszcztab(){      /*metoda pozwalajaca zniszczyc tablice, na ktora wskazuje wskaznik1*/
-    delete[] w1;
-    w1=NULL;
-    }
 void zniszcztab2(){     /*metoda pozwalajaca zniszczyc tablice, na ktora wskazuje wskaznik2*/
     delete[] w2;
     w2=NULL;
+    }
+void powieksz1(int x){  /*metoda realizujaca powiekszanie tablicy o stala wartosc*/
+    w2=w1;              /*wskaźnik w2 wskazuje teraz na stara tablice, a w1 bedzie nowa tablica*/
+    int rozmiar2=rozmiar;
+    rozmiar=rozmiar+x;  /*powiekszamy rozmiar*/
+    w1=new int[rozmiar];/*tworzymy tablice o nowym rozmiarze*/
+    for(int i=0; i<(rozmiar2); i++){
+    w1[i]=w2[i];
+    }
+    zniszcztab2();
+    }
+void powieksz2(){
+    w2=w1;              /*wskaźnik w2 wskazuje teraz na stara tablice, a w1 bedzie nowa tablica*/
+    int rozmiar2=rozmiar;
+    rozmiar=rozmiar*2;  /*powiekszamy rozmiar*/
+    w1=new int[rozmiar];/*tworzymy tablice o nowym rozmiarze*/
+    for(int i=0; i<(rozmiar2); i++){
+    w1[i]=w2[i];
+    }
+    zniszcztab2();
+    }
+public:
+tabd(){             /*konstruktor*/
+    rozmiar=10;             /*przyjmujemy rozmiar rowny 10*/
+    w1=new int[rozmiar];    /*alokujemy pamiec*/
+    }
+~tabd(){      /*destruktor*/
+    delete[] w1;
+    w1=NULL;
     }
 void wypelnijtab1(int ilosc, int j){     /*metoda pozwalajaca na wypelnienie tablicy numerem indeksu +1, w ktorej zastosowalem taktyka powiekszenia o stala*/
     for(int i=0; i<ilosc; i++){
@@ -45,46 +65,19 @@ void wypisztab(){       /*metoda, ktora wypisuje wszystkie elementy tablicy*/
     }
     cout<<endl;
     }
-void powieksz1(int x){  /*metoda realizujaca powiekszanie tablicy o stala wartosc*/
-    w2=w1;              /*wskaźnik w2 wskazuje teraz na stara tablice, a w1 bedzie nowa tablica*/
-    int rozmiar2=rozmiar;
-    rozmiar=rozmiar+x;  /*powiekszamy rozmiar*/
-    w1=new int[rozmiar];/*tworzymy tablice o nowym rozmiarze*/
-    for(int i=0; i<(rozmiar2); i++){
-    w1[i]=w2[i];
-    }
-    zniszcztab2();
-    }
-void powieksz2(){
-    w2=w1;              /*wskaźnik w2 wskazuje teraz na stara tablice, a w1 bedzie nowa tablica*/
-    int rozmiar2=rozmiar;
-    rozmiar=rozmiar*2;  /*powiekszamy rozmiar*/
-    w1=new int[rozmiar];/*tworzymy tablice o nowym rozmiarze*/
-    for(int i=0; i<(rozmiar2); i++){
-    w1[i]=w2[i];
-    }
-    zniszcztab2();
-    }
 };
 
 int main(){
 cout << "Tablica nr 1" << endl;
 tabd tab1;
-tab1.zrobtab();
 tab1.wypelnijtab1(100, 1);   /*wypelnianie tablicy 100-ma elementami z zastosowaniem powiekszania o 1*/
 tab1.wypisztab();
 cout << "Tablica nr 2" << endl;
 tabd tab2;
-tab2.zrobtab();
 tab2.wypelnijtab1(100, 500); /*wypelnianie tablicy 100-ma elementami z zastosowaniem powiekszania o 500*/
 tab2.wypisztab();
 cout << "Tablica nr 3" << endl;
 tabd tab3;
-tab3.zrobtab();
 tab3.wypelnijtab2(100);
 tab3.wypisztab();
-
-tab1.zniszcztab();          /*zwalnianie pamieci*/
-tab2.zniszcztab();
-tab3.zniszcztab();
 }
