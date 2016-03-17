2217fd85daaba982e00ec959e352474a484beb89
218492 <218492@student.pwr.edu.pl>
2016-03-17 23:03:03 +0100

    Added files via upload
    Działający program wypełniający tablicę.

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ void zniszcztab2(){     /*metoda pozwalajaca zniszczyc tablice, na ktora wskazuj
     delete[] w2;
     w2=NULL;
     }
-void wypelnijtab1(int ilosc, int j){     /*metoda pozwalajaca na wypelnienie tablicy numerem indeksu +1, w ktorej zastosowalem taktyka powiekszenia o 1*/
+void wypelnijtab1(int ilosc, int j){     /*metoda pozwalajaca na wypelnienie tablicy numerem indeksu +1, w ktorej zastosowalem taktyka powiekszenia o stala*/
     for(int i=0; i<ilosc; i++){
         if(rozmiar>i) {
         w1[i]=i+1;
@@ -28,6 +28,17 @@ void wypelnijtab1(int ilosc, int j){     /*metoda pozwalajaca na wypelnienie tab
         }
     }
     }
+void wypelnijtab2(int ilosc){           /*metoda pozwalajaca na wypelnienie tablicy numerem indeksu +1, w ktorej zastosowalem metode podwajania rozmiaru*/
+    for(int i=0; i<ilosc; i++){
+    if(rozmiar>i) {
+    w1[i]=i+1;
+    }
+    else {
+    powieksz2();
+    w1[i]=i+1;
+    }
+    }
+    }
 void wypisztab(){       /*metoda, ktora wypisuje wszystkie elementy tablicy*/
     for(int i=0; i<rozmiar; i++){
     cout<<w1[i]<<'\t';
@@ -36,9 +47,20 @@ void wypisztab(){       /*metoda, ktora wypisuje wszystkie elementy tablicy*/
     }
 void powieksz1(int x){  /*metoda realizujaca powiekszanie tablicy o stala wartosc*/
     w2=w1;              /*wskaźnik w2 wskazuje teraz na stara tablice, a w1 bedzie nowa tablica*/
+    int rozmiar2=rozmiar;
     rozmiar=rozmiar+x;  /*powiekszamy rozmiar*/
     w1=new int[rozmiar];/*tworzymy tablice o nowym rozmiarze*/
-    for(int i=0; i<(rozmiar-x); i++){
+    for(int i=0; i<(rozmiar2); i++){
+    w1[i]=w2[i];
+    }
+    zniszcztab2();
+    }
+void powieksz2(){
+    w2=w1;              /*wskaźnik w2 wskazuje teraz na stara tablice, a w1 bedzie nowa tablica*/
+    int rozmiar2=rozmiar;
+    rozmiar=rozmiar*2;  /*powiekszamy rozmiar*/
+    w1=new int[rozmiar];/*tworzymy tablice o nowym rozmiarze*/
+    for(int i=0; i<(rozmiar2); i++){
     w1[i]=w2[i];
     }
     zniszcztab2();
@@ -54,6 +76,15 @@ tab1.wypisztab();
 cout << "Tablica nr 2" << endl;
 tabd tab2;
 tab2.zrobtab();
-tab2.wypelnijtab1(100, 500); /*wypelnianie tablicy z zastosowaniem powiekszania o 500*/
+tab2.wypelnijtab1(100, 500); /*wypelnianie tablicy 100-ma elementami z zastosowaniem powiekszania o 500*/
 tab2.wypisztab();
+cout << "Tablica nr 3" << endl;
+tabd tab3;
+tab3.zrobtab();
+tab3.wypelnijtab2(100);
+tab3.wypisztab();
+
+tab1.zniszcztab();          /*zwalnianie pamieci*/
+tab2.zniszcztab();
+tab3.zniszcztab();
 }
