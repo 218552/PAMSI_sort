3c4b3387dd1aa25b08a76872b14cf1f1d1e4c036
218492 <218492@student.pwr.edu.pl>
2016-04-01 11:56:22 +0200

    test

---
+++ Zadanie1/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <ctime>
+#include "stoper.h"
+#include "tabd.h"
+using namespace std;
+
+int main(){
+int x=5000;
+cout << "Tablica nr 1" << endl;
+tabd tab1,tab2,tab3;
+tab1.przygotuj();
+tab1.wykonaj(x, 1);   /*wypelnianie tablicy x-ma elementami z zastosowaniem powiekszania o 1*/
+cout << "Tablica nr 2" << endl;
+tab2.przygotuj();
+tab2.wykonaj(x, 500); /*wypelnianie tablicy x-ma elementami z zastosowaniem powiekszania o 500*/
+cout << "Tablica nr 3" << endl;
+tab3.przygotuj();
+tab3.wykonaj(x);        /*wypelnianie tablicy x-ma elementami z zastosowaniem podwajania*/
+}
---
+++ Zadanie1/tabd.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "interfejs.h"
+#include "stoper.h"
+#ifndef TABD
+#define TABD
+class  tabd: public interfejs{            /*tworzymy klase, w ktorej bedziemy zajmowac sie nasza tablica*/
+stoper s;               /*stoper*/
+int rozmiar;            /*rozmiar tablicy*/
+int* w1;                /*wskaznik1*/
+int* w2;                /*wskaznik2*/
+void zniszcztab2();    /*metoda pozwalajaca zniszczyc tablice, na ktora wskazuje wskaznik2*/
+void powieksz1(int x); /*metoda realizujaca powiekszanie tablicy o stala wartosc*/
+void powieksz2();      /*metoda realizujaca powiekszanie tablicy przez podwojenie*/
+void wypelnijtab1(int ilosc, int j);     /*metoda pozwalajaca na wypelnienie tablicy numerem indeksu +1, w ktorej zastosowalem taktyka powiekszenia o stala*/
+void wypelnijtab2(int ilosc);           /*metoda pozwalajaca na wypelnienie tablicy numerem indeksu +1, w ktorej zastosowalem metode podwajania rozmiaru*/
+public:
+tabd();             /*konstruktor*/
+~tabd();      /*destruktor*/
+void wypisztab();                       /*metoda, ktora wypisuje wszystkie elementy tablicy*/
+virtual void wykonaj();
+virtual void wykonaj(int ilosc);
+virtual void wykonaj(int ilosc, int j);
+virtual void przygotuj();
+};
+#endif
